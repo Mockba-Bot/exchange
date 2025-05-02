c5e5e8d6f3f963c3805b54cdc8be8a600105dc98
Jame Zhan <jame@orderly.network>
2025-05-02 15:26:39 +0800

    add BaseLayout

--- app/components/orderlyProvider/index.tsx
+++ app/components/orderlyProvider/index.tsx
@@ -1,20 +1,47 @@
 import { FC, ReactNode } from "react";
 import { WalletConnectorProvider } from "@orderly.network/wallet-connector";
 import { OrderlyAppProvider } from "@orderly.network/react-app";
-import config from "@/utils/config";
+import {
+  LocaleProvider,
+  LocaleCode,
+  LocaleEnum,
+  parseI18nLang,
+} from "@orderly.network/i18n";
+import { useOrderlyConfig } from "@/hooks/useOrderlyConfig";
+import { usePathWithoutLang } from "@/hooks/usePathWithoutLang";
 
 const OrderlyProvider: FC<{ children: ReactNode }> = (props) => {
+  const config = useOrderlyConfig();
+  const path = usePathWithoutLang();
+
+  const onLanguageChanged = async (lang: LocaleCode) => {
+    window.history.replaceState({}, "", `/${lang}${path}`);
+  };
+
+  const loadPath = (lang: LocaleCode) => {
+    const _lang = parseI18nLang(lang);
+    if (_lang === LocaleEnum.en) {
+      // because en is built-in, we need to load the en extend only
+      return `/locales/extend/${_lang}.json`;
+    }
+    return [`/locales/${_lang}.json`, `/locales/extend/${_lang}.json`];
+  };
   return (
-    <WalletConnectorProvider>
-      <OrderlyAppProvider
-        brokerId="orderly"
-        brokerName="Orderly"
-        networkId="testnet"
-        appIcons={config.orderlyAppProvider.appIcons}
-      >
-        {props.children}
-      </OrderlyAppProvider>
-    </WalletConnectorProvider>
+    <LocaleProvider
+      onLanguageChanged={onLanguageChanged}
+      backend={{ loadPath }}
+    >
+      <WalletConnectorProvider>
+        <OrderlyAppProvider
+          brokerId="orderly"
+          brokerName="Orderly"
+          networkId="testnet"
+          appIcons={config.orderlyAppProvider.appIcons}
+        >
+          {props.children}
+        </OrderlyAppProvider>
+      </WalletConnectorProvider>
+    </LocaleProvider>
   );
 };
 
